dc40e746e6fcb5ea523205a3256df1cbc2c14d3c
Ethan <EthanAEdwards5@outlook.com>
2025-08-24 14:43:09 -0600

    Profile

--- ui/src/app/pages/home/profile/profile.component.ts
+++ ui/src/app/pages/home/profile/profile.component.ts
@@ -1,20 +1,19 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { IUser } from '../../../models/user.model';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
 import { MessageComponent } from '../../../shared/message/message.component';
-import ErrorType from '../../../utils/error-type.utils';
-import { isValidEmail, isValidFields } from '../../../utils/validator.utils';
+
+import { LucideAngularModule, Info } from 'lucide-angular';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
 export class ProfileComponent implements OnInit {
+  readonly infoIcon = Info;
   private authService = inject(AuthService);
-  private router = inject(Router);
 
   @ViewChild(MessageComponent) messageComponent?: MessageComponent;
 
@@ -24,54 +23,58 @@ export class ProfileComponent implements OnInit {
     this.user = this.authService.currentUserSubject.value;
   }
 
-  async updateProfile() {
-    try {
-      if (!this.user) {
-        throw new Error('No user data available to update.');
-      }
-
-      // Validate all fields
-      if (!isValidFields(this.user.firstName, this.user.lastName, this.user.email)) {
-        throw new Error('Missing fields.');
-      }
-
-      // Validate email
-      if (!isValidEmail(this.user.email)) {
-        throw new Error('Enter valid email address.');
-      }
-
-      await this.authService.updateUser(this.user);
-
-      this.handleSuccessChange('Profile updated successfully.');
-    } catch (error) {
-      this.handleErrorChange(error);
-    }
+  onUpdate(event: Event) {
+    console.log('saved', event);
   }
 
-  async onDeleteProfile() {
-    try {
-      if (!this.user || !this.user._id) {
-        throw new Error('No user data available to delete.');
-      }
+  // async updateProfile() {
+  //   try {
+  //     if (!this.user) {
+  //       throw new Error('No user data available to update.');
+  //     }
 
-      await this.authService.deleteUser();
-      this.onLogout();
-    } catch (error) {
-      this.handleErrorChange(error);
-    }
-  }
+  //     // Validate all fields
+  //     if (!isValidFields(this.user.firstName, this.user.lastName, this.user.email)) {
+  //       throw new Error('Missing fields.');
+  //     }
 
-  onLogout() {
-    this.router.navigate(['/']);
-    this.authService.logout();
-  }
+  //     // Validate email
+  //     if (!isValidEmail(this.user.email)) {
+  //       throw new Error('Enter valid email address.');
+  //     }
 
-  handleErrorChange(error: unknown) {
-    error = ErrorType(error);
-    this.messageComponent?.onMessage(error as string, 'error');
-  }
+  //     await this.authService.updateUser(this.user);
 
-  handleSuccessChange(mssg: string) {
-    this.messageComponent?.onMessage(mssg, 'error');
-  }
+  //     this.handleSuccessChange('Profile updated successfully.');
+  //   } catch (error) {
+  //     this.handleErrorChange(error);
+  //   }
+  // }
+
+  // async onDeleteProfile() {
+  //   try {
+  //     if (!this.user || !this.user._id) {
+  //       throw new Error('No user data available to delete.');
+  //     }
+
+  //     await this.authService.deleteUser();
+  //     this.onLogout();
+  //   } catch (error) {
+  //     this.handleErrorChange(error);
+  //   }
+  // }
+
+  // onLogout() {
+  //   this.router.navigate(['/']);
+  //   this.authService.logout();
+  // }
+
+  // handleErrorChange(error: unknown) {
+  //   error = ErrorType(error);
+  //   this.messageComponent?.onMessage(error as string, 'error');
+  // }
+
+  // handleSuccessChange(mssg: string) {
+  //   this.messageComponent?.onMessage(mssg, 'error');
+  // }
 }
